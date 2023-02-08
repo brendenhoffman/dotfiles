e1de5ace47406b40f5e9096bde8657e4bacc49fd
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2023-02-07 22:26:07 -0600

    re-added to be Steam Deck friendly
    These need to stay out of the .zprofile or you can't get into game mode

---
+++ .config/zsh/zshrc.d/00-env.zsh
@@ -0,0 +1,8 @@
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+
+export LESSHISTFILE="$XDG_CACHE_HOME/.lesshst"
+export CARGO_HOME="$XDG_CACHE_HOME/cargo"
+export GNUPGHOME="$XDG_CACHE_HOME/gnupg"
+export XAUTHORITY="$XDG_CACHE_HOME/.Xauthority"
