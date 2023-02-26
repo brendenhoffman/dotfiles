ebfc95ff85369ff1466663984940df90a20e2c1d
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2023-02-25 21:48:44 -0600

    Update 06-functions.zsh

--- .config/zsh/zshrc.d/06-functions.zsh
+++ .config/zsh/zshrc.d/06-functions.zsh
@@ -3,13 +3,13 @@ function swap() { mv $1 $1._tmp && mv $2 $1 && mv $1._tmp $2; }
 
 #cd and ls function
 cl() {
-	local dir="$1"
-	local dir="${dir:=$HOME}"
-	if [[ -d "$dir" ]]; then
-		cd "$dir" >/dev/null; ls
-	else
-		echo "bash: cl: $dir: Directory not found"
-	fi
+    local dir="$1"
+    local dir="${dir:=$HOME}"
+    if [[ -d "$dir" ]]; then
+        cd "$dir" >/dev/null; ls
+    else
+        echo "bash: cl: $dir: Directory not found"
+    fi
 }
 
 #Universal extract function
@@ -22,10 +22,7 @@ extract() {
         c=''
         e=1
 
-        if [[ ! -r $i ]]; then
-            echo "$0: file is unreadable: \`$i'" >&2
-            continue
-        fi
+        [[ ! -r $i ]] && echo "$0: file is unreadable: \`$i'" >&2 && continue
 
         case $i in
             *.t@(gz|lz|xz|b@(2|z?(2))|a@(z|r?(.@(Z|bz?(2)|gz|lzma|xz)))))
@@ -53,25 +50,16 @@ extract() {
 function command_not_found_handler {
     local purple='\e[1;35m' bright='\e[0;1m' green='\e[1;32m' reset='\e[0m'
     printf 'zsh: command not found: %s\n' "$1"
-    local entries=(
-        ${(f)"$(/usr/bin/pacman -F --machinereadable -- "/usr/bin/$1")"}
-    )
-    if (( ${#entries[@]} ))
-    then
-        printf "${bright}$1${reset} may be found in the following packages:\n"
+    local entries=(${(f)"$(pacman -F --machinereadable -- "$1")"})
+    (( ${#entries[@]} )) && printf "${bright}$1${reset} may be found in the following packages:\n"
         local pkg
         for entry in "${entries[@]}"
         do
             # (repo package version file)
-            local fields=(
-                ${(0)entry}
-            )
-            if [[ "$pkg" != "${fields[2]}" ]]
-            then
-                printf "${purple}%s/${bright}%s ${green}%s${reset}\n" "${fields[1]}" "${fields[2]}" "${fields[3]}"
-            fi
+            local fields=(${(0)entry})
+            [[ "$pkg" != "${fields[2]}" ]] && printf "${purple}%s/${bright}%s ${green}%s${reset}\n" "${fields[1]}" "${fields[2]}" "${fields[3]}"
             printf '    /%s\n' "${fields[4]}"
             pkg="${fields[2]}"
         done
-    fi
 }
+
