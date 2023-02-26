c7b3e500703dda6714714e145a4a70e3897f7897
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2023-02-25 21:50:19 -0600

    Update 05-dirstack.zsh

--- .config/zsh/zshrc.d/05-dirstack.zsh
+++ .config/zsh/zshrc.d/05-dirstack.zsh
@@ -1,12 +1,10 @@
 autoload -Uz add-zsh-hook
 
 DIRSTACKFILE="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/dirs"
-if [[ -f "$DIRSTACKFILE" ]] && (( ${#dirstack} == 0 )); then
-	dirstack=("${(@f)"$(< "$DIRSTACKFILE")"}")
-	[[ -d "${dirstack[1]}" ]] && cd -- "${dirstack[1]}"
-fi
+[[ -f "$DIRSTACKFILE" ]] && (( ${#dirstack} == 0 )) && dirstack=("${(@f)"$(< "$DIRSTACKFILE")"}") && [[ -d "${dirstack[1]}" ]] && cd -- "${dirstack[1]}"
+
 chpwd_dirstack() {
-	print -l -- "$PWD" "${(u)dirstack[@]}" > "$DIRSTACKFILE"
+    print -l -- "$PWD" "${(u)dirstack[@]}" > "$DIRSTACKFILE"
 }
 add-zsh-hook -Uz chpwd chpwd_dirstack
 
